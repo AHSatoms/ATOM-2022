1da05478402da3bc429f6965095637b5be0ac8de
AHSatoms <103939638+AHSatoms@users.noreply.github.com>
2022-04-22 17:31:43 -0400

    2022 Botball ICreate first program
    The ICreate follows the black line and scoops the pons.

--- main.c
+++ main.c
@@ -1,95 +1,25 @@
-#include <kipr/wombat.h>
+#include <kipr/botball.h>
 
-int RW=1;
-int LW=0;
-int ARM = 0;
-int CLAW = 1;
-int timer = 0;
+int main()
+{	
+    create_connect();
+    wait_for_light(2);
+    shut_down_in(60);
 
-void drive_forward(int ticks);
-void hard_right();
-void hard_left();
-
-int main() {
-
-    while(digital(0) == 0 ){
-        cmpc(RW);
-        cmpc(LW);
-
-        enable_servos();
-        set_servo_position(ARM,0);
-        set_servo_position(CLAW,860);
-        hard_right();
-
-        if(timer == 0){
-            hard_right();
-        }
-
-        if(timer == 1){
-            drive_forward(10573);
-        }
-
-        if(timer == 2){
-            hard_left();
-        }
-
-        if(timer == 3){
-            drive_forward(5500);
-        }
-
-        if(timer == 4){
-            set_servo_position(ARM,0);
-            set_servo_position(CLAW,860);
-        }
-
+	while (digital(0) == 0)
+    {
+    	if (analog(3) > 3850)
+        {
+    	create_drive_direct(100, 80);
+    	}
+    	else 
+        {
+    	create_drive_direct(80, 100);
+    	}
+    	
     }
-    return 0;
-}
+    create_stop ();
+    create_disconnect ();
+	return 0;
+	
 
-void drive_forward(int ticks){
-    cmpc(RW);
-    cmpc(LW);
-    while(gmpc(RW) < ticks && gmpc(LW) < ticks){
-        motor(RW,100);
-        motor(LW,100);
-        
-        if(gmpc(RW) < ticks && gmpc(LW) < ticks){
-        	timer=timer+1;
-        	ao();
-        	break;
-        }
-    }
-}
-
-void hard_right(){
-    cmpc(RW);
-    cmpc(LW);
-
-    while(gmpc(RW) < 4712 || gmpc(LW) < 2356){
-        motor(RW,-66.66);
-        motor(LW,33.33);
-        
-        if(gmpc(RW) < 4712 || gmpc(LW) < 2356){
-        	timer=timer+1;
-        	ao(); // Stop the motors
-        	break;
-        }
-    }
-        
-}
-
-void hard_left(){
-    cmpc(RW);
-    cmpc(LW);
-
-    while(gmpc(LW) < 4712 || gmpc(RW) < 2356){
-        motor(RW,33.33);
-        motor(LW,-66.66);
-        
-        if(gmpc(LW) < 4712 || gmpc(RW) < 2356){
-            timer=timer+1;
-            ao(); // Stop the motors
-            break;
-        }
-    }
-}
